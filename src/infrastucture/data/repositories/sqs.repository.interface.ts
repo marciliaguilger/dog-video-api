d6a5e463aa211a7c437626720b2c794e11775bf5
marcilia.guilger@ifood.com.br <marcilia.guilger@ifood.com.br>
2025-02-09 21:15:43 -0300

    update sqs strategy

--- src/infrastucture/data/repositories/sqs.repository.interface.ts
+++ src/infrastucture/data/repositories/sqs.repository.interface.ts
@@ -1,5 +1,6 @@
 export interface IMessageProducerRepository {
   sendMessage(body: string);
+  send(body: string);
 }
 
 export const IMessageProducerRepository = Symbol('IMessageProducerRepository');
--- src/infrastucture/data/repositories/sqs.repository.ts
+++ src/infrastucture/data/repositories/sqs.repository.ts
@@ -1,15 +1,42 @@
 import { SqsService } from '@ssut/nestjs-sqs';
+import AWS from 'aws-sdk';
 
 export class MessageProducerRepository {
-  constructor(private readonly sqsService: SqsService) {}
+  private readonly sqs: AWS.SQS;
+  
+  constructor(private readonly sqsService: SqsService) {
+    this.sqs = new AWS.SQS({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      region: process.env.AWS_REGION,
+    });
+  }
+  
   async sendMessage(body: string) {
     const message: any = JSON.stringify(body);
-
     try {
       await this.sqsService.send(process.env.QUEUE_NAME, message);
     } catch (error) {
       console.error('Error in producing message!', error);
       console.error('SqsService:', this.sqsService);
     }
+  }
+
+  async send(messageBody: string): Promise<void> {
+    const params = {
+      QueueUrl: process.env.QUEUE_NAME,
+      MessageBody: messageBody,
+    };
+
+    return new Promise((resolve, reject) => {
+      this.sqs.sendMessage(params, (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          console.log('Message sent successfully:', data.MessageId);
+          resolve();
+        }
+      });
+    });
   }
 }
