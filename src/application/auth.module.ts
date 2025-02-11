4505ba7bb346406f10fac3d872378345b9026562
marcilia.guilger@ifood.com.br <marcilia.guilger@ifood.com.br>
2025-02-10 21:22:54 -0300

    fix

--- src/application/auth.module.ts
+++ src/application/auth.module.ts
@@ -2,17 +2,19 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { AuthService } from 'src/domain/use-cases/auth/auth.service';
+import { JwtAuthGuard } from 'src/domain/use-cases/auth/jwt-auth.guard';
+import { JwtStrategy } from 'src/domain/use-cases/auth/jwt-strategy.service';
 
    @Module({
      imports: [
       PassportModule.register({ defaultStrategy: 'jwt' }),
       JwtModule.register({
         secret: 'teste',  // Replace with your secret key
-        signOptions: { expiresIn: '1h' }, // Set token expiration time
+        signOptions: { expiresIn: '60s' }, // Set token expiration time
       }),
        PassportModule,
      ],
-     providers: [AuthService],
+     providers: [AuthService, JwtStrategy, JwtAuthGuard],
      exports: [AuthService],
    })
    export class AuthModule {}
